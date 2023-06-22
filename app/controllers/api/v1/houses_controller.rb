782ba0551c5f6c1177bd2bc3b18d0b70e361fc20
Zeph180 <zephrichards1@gmail.com>
2023-06-22 18:40:54 +0300

    Create api for houses

---
+++ app/controllers/api/v1/houses_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::HousesController < ApplicationController
+  def index
+    # return houses in descending order
+    render json: House.all.order(created_at: :desc)
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
