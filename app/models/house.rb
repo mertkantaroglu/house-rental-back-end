a6d83d9d45a0f4716b4dab2abc6e24a9513195cc
LeslieAine <aineleslie@gmail.com>
2023-06-22 22:13:28 +0300

    Create reservation model

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,8 +1,20 @@
 class House < ApplicationRecord
-  validates :name, presence: true
-  validates :location, presence: true
-  validates :bedrooms, presence: true
-  validates :bathrooms, presence: true
-  validates :price, presence: true
-  validates :image, presence: true
+  belongs_to :user, class_name: 'User', foreign_key: 'users_id'
+  has_one :reservations, foreign_key: 'houses_id', dependent: :destroy
+
+  validates :name, :location, :bedrooms, :bathrooms, :price, :image_url presence: true
+
+  after_create :increment_user_houses_count
+  after_destroy :decrement_user_houses_count
+
+  private
+
+  def increment_user_houses_count
+    user.increment!(:houses_count)
+  end
+
+  def decrement_user_houses_count
+    user.decrement!(:houses_count)
+  end
+
 end
