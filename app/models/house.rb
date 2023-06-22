65704e60955360d13cefee9be14ea525bd23791b
LeslieAine <aineleslie@gmail.com>
2023-06-22 22:22:42 +0300

    Fix linters

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,7 +2,7 @@ class House < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'users_id'
   has_one :reservations, foreign_key: 'houses_id', dependent: :destroy
 
-  validates :name, :location, :bedrooms, :bathrooms, :price, :image_url presence: true
+  validates :name, :location, :bedrooms, :bathrooms, :price, :image_url, presence: true
 
   after_create :increment_user_houses_count
   after_destroy :decrement_user_houses_count
@@ -16,5 +16,4 @@ class House < ApplicationRecord
   def decrement_user_houses_count
     user.decrement!(:houses_count)
   end
-
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation < ApplicationRecord
-    belongs_to :houses, class_name: 'House'
+  belongs_to :houses, class_name: 'House'
   belongs_to :users, class_name: 'User'
 
   validates :date, :city, presence: true
