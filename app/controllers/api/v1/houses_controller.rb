4c0df9fdb6d8c7726d9176df2533994a06f7e899
Zeph180 <zephrichards1@gmail.com>
2023-06-22 21:20:23 +0300

    Implement CRUD actions on house

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -1,12 +1,37 @@
 class Api::V1::HousesController < ApplicationController
+  before_action :set_house, only: %i[show destroy]
+
   def index
     # return houses in descending order
     render json: House.all.order(created_at: :desc)
   end
 
-  def create; end
+  def create
+    house = House.create!(house_params)
+    if house
+      render json: house
+    else
+      render json: house.errors
+    end
+  end
 
-  def show; end
+  def show
+    @house = House.find(params[:id])
+    render json: @house
+  end
 
-  def destroy; end
+  def destroy
+    @house&.destroy
+    render json: { message: 'House deleted!' }
+  end
+
+  private
+
+  def house_params
+    params.permit(:name, :location, :bedrooms, :bathrooms, :price, :image)
+  end
+
+  def set_house
+    @house = House.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'houses/index'
-      post 'houses/create'
-      get 'houses/show'
-      delete 'houses/destroy'
+      post 'houses/create', to: 'houses#create'
+      get 'houses/:id', to: 'houses#show'
+      delete 'houses/destroy/:id', to: 'houses#destroy'
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
