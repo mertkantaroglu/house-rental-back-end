c4643fc3569bd34e4c34b820840ebaf9f882cc7b
Zeph180 <zephrichards1@gmail.com>
2023-06-23 12:05:24 +0300

    Change destroy routeto delete route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       get 'houses/index'
       post 'houses/create', to: 'houses#create'
       get 'houses/:id', to: 'houses#show'
-      delete 'houses/destroy/:id', to: 'houses#destroy'
+      delete 'houses/delete/:id', to: 'houses#destroy'
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
