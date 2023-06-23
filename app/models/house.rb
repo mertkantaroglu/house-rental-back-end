1a639a9b7d4cded71f37a9312dbba8a00e22e287
LeslieAine <aineleslie@gmail.com>
2023-06-23 13:45:45 +0300

    Update house model

--- app/models/house.rb
+++ app/models/house.rb
@@ -3,17 +3,4 @@ class House < ApplicationRecord
   has_one :reservations, foreign_key: 'houses_id', dependent: :destroy
 
   validates :name, :location, :bedrooms, :bathrooms, :price, :image_url, presence: true
-
-  after_create :increment_user_houses_count
-  after_destroy :decrement_user_houses_count
-
-  private
-
-  def increment_user_houses_count
-    user.increment!(:houses_count)
-  end
-
-  def decrement_user_houses_count
-    user.decrement!(:houses_count)
-  end
 end
