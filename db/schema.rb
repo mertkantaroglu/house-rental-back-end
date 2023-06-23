840c6053b1dc40f527c57b21c15595782edf2ef3
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-06-23 12:43:56 +0300

    Update schema for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_22_114447) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_093505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_114447) do
     t.integer "bathrooms", null: false
     t.decimal "price", null: false
     t.string "image", default: "https://unsplash.com/photos/2gDwlIim3Uw"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
