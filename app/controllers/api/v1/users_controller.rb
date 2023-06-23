7d7b16c0b676287db8c55d0064ba4eab27c1d2a7
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-06-23 15:35:37 +0300

    Fix linter errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,11 @@
 class Api::V1::UsersController < ApplicationController
-
   # GET /users or /users.json
   def index
     @users = User.all
   end
 
   # GET /users/1 or /users/1.json
-  def show
-  end
+  def show; end
 
   # GET /users/new
   def new
