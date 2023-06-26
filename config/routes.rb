562f97d7da6acbf75358a29680bae777f94e529e
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-06-26 19:34:56 +0300

    Update routes for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions', registrations: 'users/registrations'
+  }
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create]
