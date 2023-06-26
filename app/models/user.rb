8fb69369aaa6c4aea80d6e50b4a913fc73a49427
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-06-26 19:32:35 +0300

    Update user model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
   has_many :houses, foreign_key: 'user_id'
   has_many :reservations, foreign_key: 'user_id'
 
   validates :name, presence: true
+
+  def jwt_payload
+    super
+  end
 end
