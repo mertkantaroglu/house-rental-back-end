68fc77904697bfbdf02f3700b4c57e9e7ad22808
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-06-27 18:13:27 +0300

    Update controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,12 +18,24 @@ class Api::V1::UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
+        format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
+
+    # DELETE /users/1 or /users/1.json
+    def destroy
+      @user.destroy
+
+      respond_to do |format|
+        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    end
 
   private
 
