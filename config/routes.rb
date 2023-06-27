8e7425963ec4f2d6a18052bc39174cddcfbead0d
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-06-27 18:15:12 +0300

    Fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,6 @@ Rails.application.routes.draw do
   }
   namespace :api do
     namespace :v1 do
-      # resources :users, only: [:index, :show, :create]
-      # get 'houses/index'
-      # post 'houses/create', to: 'houses#create'
-      # get 'houses/:id', to: 'houses#show'
-      # delete 'houses/delete/:id', to: 'houses#destroy'
       resources :houses, only: [:index, :create, :show, :destroy]
       resources :reservations, only: [:index, :create, :destroy]
     end
--- db/migrate/20230622184240_create_reservations.rb
+++ db/migrate/20230622184240_create_reservations.rb
@@ -1,6 +1,10 @@
 class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
+      t.string :city
+      t.date :start_date
+      t.date :end_date
+      t.references :house, null: false, foreign_key: { to_table: :houses }
 
       t.timestamps
     end
